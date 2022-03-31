1e9579a6302be66e75980353dfa5f5d0a88fed32
andrew bui <bandrewi98@gmail.com>
2022-03-31 09:23:34 -0700

    phase 0

---
+++ backend/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const apiRouter = require('./api');
+
+const router = express.Router();
+
+router.use('/api', apiRouter);
+
+//test route
+router.get('/hello/world', function (req, res) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.send('Hello World!');
+});
+
+module.exports = router;
